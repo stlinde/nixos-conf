753ca8b8bb3e957677dd48b587a867bf8323ccb8
Sebastian Hempel Linde <sebastian@hempellinde.com>
2024-01-11 11:10:23 +0100

    changed colorscheme kitty

--- home-manager/programs/kitty/default.nix
+++ home-manager/programs/kitty/default.nix
@@ -3,7 +3,7 @@
   programs.kitty = {
     enable = true;
     font.name = "Roboto Mono";
-    theme = "Tokyo Night";
+    theme = "Rosé Pine";
     settings = {
       window_padding_width = 10;
       window_padding_height = 10;
